98cf2aea8770b0cf303213bd4332bbf7355bec7c
Reeve <reevelau@gmail.com>
2016-08-27 19:53:35 +0800

    adding basic function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,124 @@
-require('babel-polyfill');
-require('babel-register');
+'use strict';
+
+var debug = require('debug')('main');
+var config = require('config');
+var express = require('express');
+var bodyParser = require('body-parser');
+var Queue = require('co-queue');
+var co = require('co');
+
+var NodeGeocoder = require('node-geocoder');
+
+var Spawn = require('./poke-spawn.js');
+var SpawnPointInfo = require('./spawn-point-info.js');
+var PokemonInfo = require('./pokemon-info.js');
+var Messenger = require('./telegram-bot-messenger.js');
+
+var alarmConfig = config.get('alarms')[0];
+
+var spawnpointfile = 'spawn_point_store.json';
+
+var spawnPointStore = new SpawnPointInfo(spawnpointfile);
+spawnPointStore.initialize().then(success=>{
+  debug(`[+] spawn point file loaded`)
+});
+
+var pokeInfo = new PokemonInfo();
+pokeInfo.initialize('./config/default.json', 'zh_hk').then(()=>{
+  debug(`[+] reading pokemon information done`);
+});
+
+var options = {
+  provider: 'google',
+  // Optional depending on the providers
+  httpAdapter: 'https', // Default
+  language: 'zh-TW'
+};
+var geocoder = NodeGeocoder(options);
+
+var bot = new Messenger(
+  {
+    bot_token:alarmConfig.bot_token,
+    chat_id : alarmConfig.chat_id
+  }
+);
+
+var queue = new Queue();
+
+co(function*(){
+  while(true){
+
+    var body = yield queue.next();
+
+    if(body.message.pokemon_id )
+    {
+      var pokeId = body.message.pokemon_id;
+      var enable = pokeInfo.isEnabled(pokeId);
+
+      var pokemon = {
+        id : pokeId,
+        name: pokeInfo.getPokemonName(pokeId),
+        enable : enable,
+        latitude: body.message.latitude,
+        longitude: body.message.longitude,
+        geoCoderAddr: yield geocoder.reverse({lat:body.message.latitude, lon:body.message.longitude}),
+        last_modified_time: body.message.last_modified_time,
+        time_until_hidden_ms: body.message.time_until_hidden_ms,
+        spawnpointId: parseInt(body.message.spawnpoint_id,16),
+        message: ''
+      };
+
+      if(pokemon.enable){
+        var timeInfo = '';
+
+        var spawnp = spawnPointStore.getSpawnPointById(pokemon.spawnpointId);
+        if(spawnp){
+          var spawn = new Spawn(pokemon.last_modified_time, spawnp.spawntime,  spawnp.type);
+          timeInfo = spawn.toString();
+        }
+        else{
+          //TODO!
+          // hack
+          var spawntime = pokemon.last_modified_time + 738310
+          timeInfo = 'NOT FOUND';
+        }
+
+        pokemon.message = `${pokemon.name} (${pokemon.id}) @${pokemon.geoCoderAddr[0].streetName}\n${timeInfo}`;
+        yield bot.handleSpawn(pokemon);
+        debug(`[+] ${pokemon.name} notification was triggered.`)
+      }
+      else{
+        debug(`[+] ${pokemon.name} ignored: notify not enabled.`);
+      }
+
+    }
+  }
+
+}).catch(onerror);
+
+function onerror(err) {
+  // log any uncaught errors
+  // co will not throw any errors you do not handle!!!
+  // HANDLE ALL YOUR ERRORS!!!
+  console.error(err.stack);
+}
+
+
+var port = 3000;
+var app = express();
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+  res.send('hello world');
+});
+
+app.post('/', function(req,res){
+  var body = req.body;
+  process.stdout.write('.');
+  queue.push(body);
+  res.send('OK');
+});
+
+app.listen(port);
---
+++ src/telegram-bot-messenger.js
@@ -0,0 +1,59 @@
+'use strict';
+var debug = require('debug')('telegram-bot-messenger');
+
+
+var TelegramBot = require('node-telegram-bot-api');
+var telegram_token = '239593793:AAHw32Ug9h3X2n9K5tn9gJpVBVqFxr30Vag';
+var telegram_chat_id = '@twcustome1';
+
+class TelegramBotMessenger{
+  constructor(config){
+    /*
+    {
+			"active": "True",
+			"type":"telegram",
+			"bot_token":"239593793:AAHw32Ug9h3X2n9K5tn9gJpVBVqFxr30Vag",
+			"chat_id":"@twcustome1",
+			"title": "<pkmn> (<id>) <addr>",
+			"url": "<gmaps>",
+			"body": "to <12h_time> (<time_left> remains)!"
+		}
+    */
+    debug('[+] creating telegram bot');
+    this.bot_token = config.bot_token || '239593793:AAHw32Ug9h3X2n9K5tn9gJpVBVqFxr30Vag';
+    this.chat_id = config.chat_id || '@twcustome1';
+    this.bot = new TelegramBot(telegram_token, {polling: false});
+    debug('[+] creating telegram bot done');
+  }
+
+
+
+  handleSpawn(pokemon){
+    debug('[+] handle pokemon spawn');
+
+    return new Promise(function(resolve,reject){
+      this.bot.sendLocation(this.chat_id, pokemon.latitude, pokemon.longitude)
+        .then((success)=>{
+          debug(`[+] finish sending map`);
+
+          this.bot.sendMessage(this.chat_id, pokemon.message)
+            .then(success=>{
+              debug(`[+] finish sending message`);
+              resolve(success);
+            },error=>{
+              debug(`[-] failed sending message ${error}`);
+              reject(error);
+            });
+
+        },
+        (failed)=>{
+          debug(`[-] failed sending map ${error}`);
+          reject(error);
+        }
+      );
+
+    }.bind(this));
+  }
+};
+
+module.exports = TelegramBotMessenger;
